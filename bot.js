61276c011d729aa2f73314c909ef149cd3a61d13
Niie <43474785+NiieDev@users.noreply.github.com>
2020-02-25 19:09:48 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,11 @@ client.on('message', message => {
         };
         request.get(options, function(error, response, body) {
             const jsonobj = JSON.parse(body);
-            console.log("Body: " + jsonobj.data.id);
+            if(jsonobj.hasOwnProperty('data')){
+                console.log("Body: " + jsonobj.data.id);
+            }else{
+                console.log("error: not exists");
+            }
         });
     }
 });
